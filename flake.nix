46dc15a517f682928deff34ebb49d84d2de4a764
Michael Hueschen <m@mhueschen.space>
2021-09-29 14:11:14 -0600

    attempt to build it

--- flake.nix
+++ flake.nix
@@ -4,20 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
   flake-utils.lib.eachSystem ["x86_64-linux"] (system:
   {
     devShell =
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [ rust-overlay.overlay ];
         };
       in
       pkgs.mkShell {
         buildInputs = with pkgs; [
-          cowsay
+          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          pkg-config
+          xorg.libxcb
+          xorg.libX11
+          libjack2
+          qjackctl
         ];
       };
   });
